005752f4e91b0a0516a57ab308a0a131904b8a1e
Mashiat Islam <mashiath342@gmail.com>
2025-07-05 02:16:51 +0600

    working on socket

--- index.js
+++ index.js
@@ -19,29 +19,51 @@ app.get('/', (req, res) => {
   res.send('Socket.io server is running!');
 });
 
+// Track users in each room
+const roomUsers = new Map(); // roomId => Set of socket ids
+
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    if (!roomUsers.has(roomId)) {
+      roomUsers.set(roomId, new Set());
+    }
+    const users = roomUsers.get(roomId);
+    users.add(socket.id);
+
+    // Send room info to the joining user
+    socket.emit('room-info', {
+      isOfferer: users.size === 1,
+      existingUsers: [...users].filter(id => id !== socket.id)
+    });
   });
 
-  // Example: echo event
-  socket.on('echo', (data) => {
-    socket.emit('echo', data);
+  socket.on('disconnecting', () => {
+    // Remove user from all rooms they are in
+    for (const roomId of socket.rooms) {
+      if (roomUsers.has(roomId)) {
+        const users = roomUsers.get(roomId);
+        users.delete(socket.id);
+        if (users.size === 0) {
+          roomUsers.delete(roomId);
+        }
+      }
+    }
   });
 
-  // WebRTC signaling events
-  socket.on("webrtc-offer", (offer) => {
-    socket.broadcast.emit("webrtc-offer", offer);
+  // WebRTC signaling events (broadcast to room except sender)
+  socket.on('webrtc-offer', ({ roomId, offer }) => {
+    socket.to(roomId).emit('webrtc-offer', { from: socket.id, offer });
   });
 
-  socket.on("webrtc-answer", (answer) => {
-    socket.broadcast.emit("webrtc-answer", answer);
+  socket.on('webrtc-answer', ({ roomId, answer }) => {
+    socket.to(roomId).emit('webrtc-answer', { from: socket.id, answer });
   });
 
-  socket.on("webrtc-ice-candidate", (candidate) => {
-    socket.broadcast.emit("webrtc-ice-candidate", candidate);
+  socket.on('webrtc-ice-candidate', ({ roomId, candidate }) => {
+    socket.to(roomId).emit('webrtc-ice-candidate', { from: socket.id, candidate });
   });
 });
 
