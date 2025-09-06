12dac6e529466fb4cc6fdd30c28361371a4c13a4
Mashiat Islam <mashiath342@gmail.com>
2025-09-06 23:18:33 +0600

    recreation

--- index.js
+++ index.js
@@ -94,6 +94,7 @@ socket.on('disconnecting', () => {
 socket.on('webrtc-offer', ({ to, from, offer }) => {
   console.log(`WebRTC offer from ${from} to ${to}`);
   const targetSocketId = getSocketIdUsingUid(to);
+  console.log(`WebRTC offer from ${from} to targetSocketId ${targetSocketId}`);
   if (targetSocketId) {
     socket.to(targetSocketId).emit('webrtc-offer', { from, offer }); // keep `from` as userId
   }
